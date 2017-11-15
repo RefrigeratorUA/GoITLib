62ea7d4c2d3cfe096e01c939f071ff33d07f0794
RefrigeratorUA <shalenyi.dm@gmail.com>
2017-11-15 12:04:22 +0200

    Angular 4 - Блок 6. Теория. Сервисы

---
+++ Angular4/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CarsService } from './cars.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+
+export class AppComponent implements OnInit {
+
+  cars = [];
+
+  constructor(private service: CarsService) { }
+
+  ngOnInit() {
+    this.cars = this.service.cars;
+  }
+}
