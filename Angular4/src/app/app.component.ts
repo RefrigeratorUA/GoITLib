df36701c870c5743c985384fb8cf00d42e7f67eb
RefrigeratorUA <shalenyi.dm@gmail.com>
2017-12-07 11:34:23 +0200

    Angular 4 - Блок 7. Теория. Формы

--- Angular4/src/app/app.component.ts
+++ Angular4/src/app/app.component.ts
@@ -1,18 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { CarsService } from './cars.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit {
 
-  cars = [];
+  answers = [
+    {
+      type: 'yes',
+      text: 'Да'
+    },
+    {
+      type: 'no',
+      text: 'Нет'
+    }
+  ];
 
-  constructor(private service: CarsService) { }
+  form: FormGroup;
+
+  charsCount = 4;
 
   ngOnInit() {
-    this.cars = this.service.cars;
+    this.form = new FormGroup({
+      user: new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email], [this.checkForEmail]),
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)])
+      }),
+      country: new FormControl('ru'),
+      answer: new FormControl('no')
+    });
   }
+
+  checkForLength(control: FormControl) {
+    if (control.value.length <= this.charsCount) {
+      return {
+        'lengthError': true
+      }
+    }
+    return null
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'q@w') {
+          resolve({
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null)
+        }
+      }, 3000)
+    })
+  }
+
+  onSubmit() {
+    console.log('Submited!', this.form);
+  }
+
 }
--- Angular4/src/app/app.module.ts
+++ Angular4/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CarComponent } from './car/car.component';
-import { CarAddComponent } from './car-add/car-add.component';
-import { CarsService } from './cars.service';
-import { ConsoleService } from './console.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CarComponent,
-    CarAddComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
-  providers: [CarsService, ConsoleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
